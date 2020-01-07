0898e8e59781cb9e34f854d7749ba5534b7be242
InnaPodkosova <innapodkosova@gmail.com>
2020-01-07 15:32:09 +0200

    new-test

---
+++ js/tab-accordio.js
@@ -0,0 +1,15 @@
+// let bussContent = document.querySelector('#content .card');
+// let i = 0;
+//
+//     bussContent.onclick = function () {
+//         bussContent.className = '';
+//         i = i - 1;
+//
+//         if(i < 0){
+//             i = bussContent.length -1;
+//         }
+//
+//         bussContent.className = 'active-ac';
+//
+//
+// };
