5ced7fcaf60f141fdd421a943dc0a84e37e528d0
Inna <inna@lagoon.me>
2020-02-05 17:03:04 +0200

    add new gallery

--- js/main.js
+++ js/main.js
@@ -71,6 +71,47 @@
     });
 
 
+
+    $('.works-slideshow .team-item').each(function() {
+        var slider = $(this);
+        slider.slick({
+            arrows: true,
+            dots: true,
+            accessibility: false,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [{
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+
+        var sLightbox = $(this);
+        sLightbox.slickLightbox({
+            src: 'src',
+            itemSelector: '.team-image img'
+        });
+    });
+
+
+
+
+
+
 })(jQuery);
 
 
