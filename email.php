b41aef7c87a02ad28d4a135cf2636ef230065787
InnaPodkosova <innapodkosova@gmail.com>
2020-06-21 21:10:21 +0300

    test and add form email.php v11

--- email.php
+++ email.php
@@ -2,6 +2,7 @@
 <html>
 <head>
    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1">
    <title>Ваше сообщение успешно отправлено</title>
    <link rel="stylesheet" href="mail.css?v2">
 </head>
