72378e02a716143db83c5dc42ef692611708a471
InnaPodkosova <innapodkosova@gmail.com>
2020-06-21 20:42:08 +0300

    test and add form email.php v6

--- email.php
+++ email.php
@@ -14,11 +14,11 @@
    $back = "<p><a href=\"javascript: history.back()\">Вернуться назад</a></p>";
 
    if(!empty($_POST['name']) and !empty($_POST['phone']) and !empty($_POST['email'])
-   and !empty($_POST['dataevents'])){
+   and !empty($_POST['data'])){
       $name = trim(strip_tags($_POST['name']));
       $phone = trim(strip_tags($_POST['phone']));
       $mail = trim(strip_tags($_POST['email']));
-      $message = trim(strip_tags($_POST['dataevents']));
+      $message = trim(strip_tags($_POST['data']));
 
       mail('podfox.events@gmail.com', 'podfox-events.com',
       'Вам написал: '.$name.'<br />Его номер: '.$phone.'<br />Его почта: '.$mail.'<br />
