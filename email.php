22dcc4b634c6be701a5045ca7f52a521f0c65e0e
InnaPodkosova <innapodkosova@gmail.com>
2020-06-22 23:00:29 +0300

    add descrip

--- email.php
+++ email.php
@@ -4,7 +4,7 @@
    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <title>Ваше сообщение успешно отправлено</title>
-   <link rel="stylesheet" href="mail.css?v2">
+   <link rel="stylesheet" href="mail.css?v3">
 </head>
 
 <body>
